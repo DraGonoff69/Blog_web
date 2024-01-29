a07011e32bc9b36cd7b0b63949782f1cfb886864
Ayush Mishra <100191388+DraGonoff69@users.noreply.github.com>
2024-01-29 15:21:35 +0530

    Blog_Web_first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  console.log(import.meta.env.VITE_API_URL);
+  return (
+    <>
+    <h1>A blog Webpage</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,6 @@
+const conf={
+    appwriteUrl:String(process.env.VITE_APPWRITE_URL),
+}
+
+
+export default conf
